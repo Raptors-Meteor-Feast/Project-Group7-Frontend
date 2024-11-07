cca8a685a866b630d098e2ac98de9a8bcf04ef4e
tenmonster <tenmonster@hotmail.com>
2024-11-07 14:24:51 +0700

    add router

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+mockData = {
+    id: 1,
+    categorie:'Assasincreed',
+    price: 200
+}
+
+const Card = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NextUIProvider>
-    <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </StrictMode>,
 )
