44f6fe25ef1ff906b814ac4a449ceb8c20cd1a34
Kulsayas <Kulsaya.see@gmail.com>
2024-11-08 09:29:16 +0700

    added component Banner, Thumbnail search and Crousel, BaseButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
+import Thumbnail from "./components/CrouselThumbnail";
+import SearchThumbnail from "./components/SearchThumbnail";
+import CrouselBanner from "./components/CrouselBanner";
 
 const App = () => {
   return (
     <div>
-      
+      <CrouselBanner />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
