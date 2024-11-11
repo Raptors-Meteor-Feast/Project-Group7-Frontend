336a214abab42ddb818e595d0d2869c04a914a89
Kulsayas <Kulsaya.see@gmail.com>
2024-11-11 15:03:05 +0700

    Edit Carosel Image

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import Home from './pages/Home';
-import CardComponent from './components/Card-Home/CardComponent';
-import CardDetail from './components/Card-Detail/CardDetail';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+// import CardComponent from "./components/Card-Home/CardComponent";
+import CardDetail from "./components/Card-Detail/CardDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <Home />,
   },
   {
     path: "/card/:id",
-    element: <CardDetail />
+    element: <CardDetail />,
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
       <RouterProvider router={router} />
     </NextUIProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import BoxFiveCard from '../components/Card-Home/BoxFiveCard'
-import BoxThreeContainer from '../components/Container-Home/BoxThreeContainer'
-import Hero from '../components/Hero-Home/Hero'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer/Footer'
+import React from "react";
+import BoxFiveCard from "../components/Card-Home/BoxFiveCard";
+import BoxThreeContainer from "../components/Container-Home/BoxThreeContainer";
+import Hero from "../components/Hero-Home/Hero";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer/Footer";
+import CaroselImage from "../components/Card-Detail/CaroselImage";
 
 const Home = () => {
-    return (
-        <div>
-            <Nav/>
-            <Hero />
-            <BoxFiveCard name="Promotion"/>
-            <BoxFiveCard name="Most Popular"/>
-            <BoxThreeContainer/>
-            <Footer/>
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* <Nav />
+      <Hero />
+      <BoxFiveCard name="Promotion" />
+      <BoxFiveCard name="Most Popular" />
+      <BoxThreeContainer />
+      <Footer /> */}
+      <CaroselImage />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
